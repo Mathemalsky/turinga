9002b49021a636477f1ef2a07d59fc60d04d6025
Max Jenke <max.jenke@ovgu.de>
2021-02-19 10:05:46 +0100

    Adding new rotate function

--- src/turinga.cpp
+++ src/turinga.cpp
@@ -23,90 +23,51 @@ using Bytes = std::vector<unsigned char>;
 // rotates the wheels,
 // each wheel rotates depending on the position of the prvious wheel
 void rotate(Byte* rotorShifts, const size_t length) {
-  __m256i_u b = _mm256_setzero_si256();
+  __m256i low_4_bits_mask  = _mm256_set1_epi8(0b00001111);
+  __m256i high_4_bits_mask = _mm256_set1_epi8(0b11110000);
+  __m256i table            = _mm256_setr_epi8(
+    0b0000, 0b0001, 0b1001, 0b1110, 0b1101, 0b1011, 0b0111, 0b0110, 0b1111, 0b0010, 0b1100, 0b0101,
+    0b1010, 0b0100, 0b0011, 0b1000, 0b0000, 0b0001, 0b1001, 0b1110, 0b1101, 0b1011, 0b0111, 0b0110,
+    0b1111, 0b0010, 0b1100, 0b0101, 0b1010, 0b0100, 0b0011, 0b1000);
+  __m256i rotor_intervals = _mm256_setr_epi8(
+    1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49,
+    51, 53, 55, 57, 59, 61, 63);
+  __m256i factor = _mm256_setr_epi8(
+    0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111,
+    0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111,
+    0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111,
+    0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111, 0b00000000, 0b11111111);
 
-  __m256i_u a = _mm256_loadu_si256((__m256i_u*) rotorShifts);
+  __m256i values = _mm256_loadu_si256((__m256i*) rotorShifts);
+  __m256i y      = _mm256_and_si256(values, low_4_bits_mask);
+  __m256i x      = _mm256_and_si256(values, high_4_bits_mask);
 
-  Byte last = 1;
-  b                  = _mm256_insert_epi8(b, last, 0);
-  last               = !((rotorShifts[0] + last) % 3);
-  b                  = _mm256_insert_epi8(b, last, 1);
-  last               = !((rotorShifts[1] + last) % 5);
-  b                  = _mm256_insert_epi8(b, last, 2);
-  last               = !((rotorShifts[2] + last) % 7);
-  b                  = _mm256_insert_epi8(b, last, 3);
-  last               = !((rotorShifts[3] + last) % 9);
-  b                  = _mm256_insert_epi8(b, last, 4);
-  last               = !((rotorShifts[4] + last) % 11);
-  b                  = _mm256_insert_epi8(b, last, 5);
-  last               = !((rotorShifts[5] + last) % 13);
-  b                  = _mm256_insert_epi8(b, last, 6);
-  last               = !((rotorShifts[6] + last) % 15);
-  b                  = _mm256_insert_epi8(b, last, 7);
-  if(length>8) {
-    last = !((rotorShifts[7] + last) % 17);
-    b    = _mm256_insert_epi8(b, last, 8);
-    last = !((rotorShifts[8] + last) % 19);
-    b    = _mm256_insert_epi8(b, last, 9);
-    last = !((rotorShifts[9] + last) % 21);
-    b    = _mm256_insert_epi8(b, last, 10);
-    last = !((rotorShifts[10] + last) % 23);
-    b    = _mm256_insert_epi8(b, last, 11);
-    last = !((rotorShifts[11] + last) % 25);
-    b    = _mm256_insert_epi8(b, last, 12);
-    last = !((rotorShifts[12] + last) % 27);
-    b    = _mm256_insert_epi8(b, last, 13);
-    last = !((rotorShifts[13] + last) % 29);
-    b    = _mm256_insert_epi8(b, last, 14);
-    last = !((rotorShifts[14] + last) % 31);
-    b    = _mm256_insert_epi8(b, last, 15);
-    if(length>16) {
-      last = !((rotorShifts[15] + last) % 33);
-      b    = _mm256_insert_epi8(b, last, 16);
-      last = !((rotorShifts[16] + last) % 35);
-      b    = _mm256_insert_epi8(b, last, 17);
-      last = !((rotorShifts[17] + last) % 37);
-      b    = _mm256_insert_epi8(b, last, 18);
-      last = !((rotorShifts[18] + last) % 39);
-      b    = _mm256_insert_epi8(b, last, 19);
-      last = !((rotorShifts[19] + last) % 41);
-      b    = _mm256_insert_epi8(b, last, 20);
-      last = !((rotorShifts[20] + last) % 43);
-      b    = _mm256_insert_epi8(b, last, 21);
-      last = !((rotorShifts[21] + last) % 45);
-      b    = _mm256_insert_epi8(b, last, 22);
-      last = !((rotorShifts[22] + last) % 47);
-      b    = _mm256_insert_epi8(b, last, 23);
-      if (length>24) {
-        last = !((rotorShifts[23] + last) % 49);
-        b    = _mm256_insert_epi8(b, last, 24);
-        last = !((rotorShifts[24] + last) % 51);
-        b    = _mm256_insert_epi8(b, last, 25);
-        last = !((rotorShifts[25] + last) % 53);
-        b    = _mm256_insert_epi8(b, last, 26);
-        last = !((rotorShifts[26] + last) % 55);
-        b    = _mm256_insert_epi8(b, last, 27);
-        last = !((rotorShifts[27] + last) % 57);
-        b    = _mm256_insert_epi8(b, last, 28);
-        last = !((rotorShifts[28] + last) % 59);
-        b    = _mm256_insert_epi8(b, last, 29);
-        last = !((rotorShifts[29] + last) % 61);
-        b    = _mm256_insert_epi8(b, last, 30);
-        last = !((rotorShifts[30] + last) % 63);
-        b    = _mm256_insert_epi8(b, last, 31);
-      }
-    }
-  }
+  // function pi(y)
+  y = _mm256_shuffle_epi8(table, y);
 
-    a = _mm256_add_epi8(a, b);
+  x = _mm256_srli_epi32(x, 4);
 
-    _mm256_storeu_si256((__m256i_u*) rotorShifts, a);
+  // function <x,y>
+  y = _mm256_and_si256(x, y);
 
-    rotorShifts[0] += !(rotorShifts[length - 1] % (2 * length + 1));
+#ifdef __AVX512VL__
+  y = _mm256_popcnt_epi8(y);
+#else
+  __m256i lookup = _mm256_setr_epi8(
+    0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+  y = _mm256_shuffle_epi8(lookup, y);
+#endif
+
+  y      = _mm256_shuffle_epi8(factor, y);
+  y      = _mm256_and_si256(y, rotor_intervals);
+  values = _mm256_add_epi8(values, y);
+
+  _mm256_storeu_si256((__m256i*) rotorShifts, values);
 }
+
 TuringaKey generateTuringaKey(const size_t keylength, const std::string& availableRotors) {
   std::vector<char> rotorNames(keylength);
-  Byte* rotorShifts = (Byte*) malloc(MAX_KEYLENGTH);
+  Byte* rotorShifts           = (Byte*) malloc(MAX_KEYLENGTH);
   const size_t numberOfRotors = availableRotors.length();
   duthomhas::csprng random;
   for (size_t i = 0; i < keylength; ++i) {
@@ -127,12 +88,12 @@ void encrypt(Data& bytes, TuringaKey& key, const Byte* rotors) {
   // this is an empirical constant from kcachgrind measurement
   // it's ratio of time spend rotate and time spend in encryption
   // the second thread gives speed up up to 13% more would be close to useless
-  const double rotateRatio = 45.92/53.71;
+  const double rotateRatio = 45.92 / 53.71;
   size_t begin             = 0, end;
 
-  end = std::round(bytes.size * (1-((1-rotateRatio)/(2-rotateRatio))));
+  end = std::round(bytes.size * (1 - ((1 - rotateRatio) / (2 - rotateRatio))));
 
-  Byte* rotorShifts_cpy = (Byte*)malloc(MAX_KEYLENGTH);
+  Byte* rotorShifts_cpy = (Byte*) malloc(MAX_KEYLENGTH);
   std::memcpy(rotorShifts_cpy, key.rotorShifts, MAX_KEYLENGTH);
   TuringaKey key_cpy{key.direction, key.length, key.rotorNames, rotorShifts_cpy, key.fileShift};
 
@@ -141,7 +102,7 @@ void encrypt(Data& bytes, TuringaKey& key, const Byte* rotors) {
   for (size_t i = 0; i < end; ++i) {
     rotate(key.rotorShifts, key.length);
   }
-  encrypt_block(bytes,key,rotors,end,bytes.size);
+  encrypt_block(bytes, key, rotors, end, bytes.size);
 
   thr.join();
   free(key_cpy.rotorShifts);
