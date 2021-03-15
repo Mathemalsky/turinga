3b68a1d41e434e83554266b1410ef17c35f2be28
Mathemalsky <>
2021-03-15 22:30:49 +0100

    continiued documenting bent function

--- include/turinga.hpp
+++ include/turinga.hpp
@@ -71,7 +71,11 @@ struct TuringaKey {
  * \f{equation*}{ (v_0,...,v_{2n-1}) := r\f}
  * Furthermore we define a binary vector s of length n by
  * \f{equation*} s_i := <v_i, \pi(v_{2n-1-i})> \qquad i \in \{0,...,n-1\}\f}, where
- * \f$\pi\f$ is defined by
+ * \f$\pi\f$ is the bijective mapping from \f$\mathbb{F}_2^4\f$ to \f$\mathbb{F}_2^4\f$
+ * which we obtain by considering the input vector as a coefficient vector of a binary
+ * polynomial \f$g\f$ with \f$\deg(g) < 4\f$.
+ * Then the output vector is the coeficcient vector of \f{equation*}{g^{-1}(x) \mod x^4 + x + 1\f}
+ *
  * \authors Max, Jurek
  * \param fileShifts determines the current substitution
  * \param length number of rotors in use
