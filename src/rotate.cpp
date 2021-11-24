4a4834004aa2249549d0c1aa344c01779945f5c0
Mathemalsky <jurek.rostalsky@ovgu.de>
2021-11-24 16:08:00 +0100

    remove fixpoints from rotate

--- src/rotate.cpp
+++ src/rotate.cpp
@@ -70,6 +70,10 @@ void rotate(Byte* rotorShifts) {
   __m256i z = _mm256_and_si256(x, y);              // elementwise and
   z         = _mm256_shuffle_epi8(lookup_sum, z);  // sum over all elements using the lookup sum
 
+  // if the first entry is 0 invert enverything
+  __m256i mode = _mm256_set1_epi8(_mm256_extract_epi8(z, 0));
+  z            = _mm256_cmpeq_epi8(z, mode);
+
   // set each position of z to 0 or rotor intervals and add to values
   z              = _mm256_and_si256(z, rotor_intervals);
   __m256i values = _mm256_set_m128i(b, a);      // concatinate a and b to obtain initial rotor shifts
@@ -97,9 +101,6 @@ void rotate(Byte* rotorShifts) {
   const __m128i rotor_intervals_1 = _mm_setr_epi8(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31);
   const __m128i rotor_intervals_2 = _mm_setr_epi8(33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63);
   const __m128i shuffle_high      = _mm_setr_epi8(8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7);
-  const __m128i all_one           = _mm_setr_epi8(
-    0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111,
-    0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111);
 // enable gcc warning -Woverflow
 #pragma GCC diagnostic pop
 
@@ -161,13 +162,10 @@ void rotate(Byte* rotorShifts) {
   z1         = _mm_shuffle_epi8(lookup_sum, z1);
   z2         = _mm_shuffle_epi8(lookup_sum, z2);
 
-  // extend the first entry to all the __m128i and if it's 0 invert enverything
-  Byte* chars = (Byte*) malloc(16);
-  _mm_storeu_si128((__m128i*) chars, z1);
-  __m128i mode = _mm_set1_epi8(chars[0]);
-  mode         = _mm_xor_si128(all_one, mode);
-  z1           = _mm_xor_si128(mode, z1);
-  z2           = _mm_xor_si128(mode, z2);
+  // if the first entry is 0 invert enverything
+  __m128i mode = _mm_set1_epi8(_mm_extract_epi8(z1, 0));
+  z1           = _mm_cmpeq_epi8(z1, mode);
+  z2           = _mm_cmpeq_epi8(z2, mode);
 
   // multyply the indicatorvariable z with the shifts
   z1 = _mm_and_si128(z1, rotor_intervals_1);
@@ -203,10 +201,6 @@ void rotate(Byte* rotorShifts) {
   table[14]   = 0b0011;
   table[15]   = 0b1000;
 
-  Byte* modeTable = (Byte*) malloc(2);
-  modeTable[0]    = 0b00000001;
-  modeTable[1]    = 0b00000000;
-
   Byte* x = (Byte*) malloc(MAX_KEYLENGTH * 2);
   for (size_t i = 0; i < MAX_KEYLENGTH; i++) {
     x[2 * i]     = rotorShifts[i] & 0b00001111;  // the rightmost bits
@@ -215,7 +209,7 @@ void rotate(Byte* rotorShifts) {
 
   ++rotorShifts[0];
   Byte mode = table[x[0]] & x[2 * MAX_KEYLENGTH - 1];
-  mode      = modeTable[__builtin_popcount(mode) & 0b00000001];
+  mode      = !(__builtin_popcount(mode) & 0b00000001);
 
   for (size_t i = 1; i < MAX_KEYLENGTH; i++) {
     Byte val = table[x[i]];
