6769572a4b129a2c3e29d25b1dac0c734219079f
Mathemalsky <jurek.rostalsky@ovgu.de>
2022-02-20 14:25:54 +0100

    adjust syntax help to os

--- src/errors.cpp
+++ src/errors.cpp
@@ -1,6 +1,6 @@
 /*
  * Turinga is a simple symmetric encryption scheme based on ideas from enigma.
- * Copyright (C) 2021  Mathemalsky, MilchRatchet
+ * Copyright (C) 2022  Mathemalsky, MilchRatchet
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -34,7 +34,7 @@ void InappropriateNumberOfArguments::what() {
   print_lightred("ERROR: ");
   std::cout << "Inapropriate number of arguments in function <" << p_func << ">. Got " << p_number << " but expectet "
             << p_expected << ".\n";
-  std::cout << "Type <./" << PROJECTNAME << "> <help> for syntax help.\n";
+  std::cout << "Type " << EXECUTE << " <help> for syntax help.\n";
   exit(-1);
 }
 
@@ -47,7 +47,7 @@ void InvalidArgument::what() {
   std::cout << timestamp(current_duration());
   print_lightred("ERROR: ");
   std::cout << "Argument <" << p_arg << "> is invaild " << p_setting << ".\n";
-  std::cout << "Type <./" << PROJECTNAME << "> <help> for syntax help.\n";
+  std::cout << "Type " << EXECUTE << " <help> for syntax help.\n";
   exit(-1);
 }
 
@@ -86,7 +86,7 @@ void NoKey::what() {
 
 void syntax() {
   std::cout << "Syntax\n======\n";
-  std::cout << "./" << PROJECTNAME << " <argument1> <argument2> ...\n";
+  std::cout << EXECUTE << " <argument1> <argument2> ...\n";
   std::cout << "Valid options are:\n";
   syntaxCrypt();
   syntaxGenerateKey();
@@ -94,7 +94,7 @@ void syntax() {
 }
 
 void syntaxCrypt() {
-  std::cout << "- ./" << PROJECTNAME << " crypt <input_file> <key> <rotors> <output_file>\n";
+  std::cout << "- " << EXECUTE << " crypt <input_file> <key> <rotors> <output_file>\n";
   std::cout << "    input_file  : path and filename (with ending) of the file to be "
                "encrypted/decrypted\n";
   std::cout << "    key         : path and filename (without ending) of the key used to be "
@@ -102,7 +102,7 @@ void syntaxCrypt() {
   std::cout << "    rotors      : path to the directory where the rotor files are stored\n";
   std::cout << "    output_file : path and filename (with ending) to write the file into\n";
 
-  std::cout << "- ./" << PROJECTNAME << " <input_file>\n";
+  std::cout << "- " << EXECUTE << " <input_file>\n";
   std::cout << "    input_file  : path and filename (with ending) of the file to be encrypted\n";
   std::cout << "                  <key> is assumed to be default, which is <" << STD_KEY_DIR << STD_KEY << ">.\n";
   std::cout << "                  If this file does not exist, a new pair of keys potentially "
@@ -112,18 +112,18 @@ void syntaxCrypt() {
             << "> is empty all Rotors with valid names will be generated there in before generating the keys.\n";
   std::cout << "                  <output_file> is assumed to be <input_file> appended by the suffix <.tur>.\n";
 
-  std::cout << "- ./" << PROJECTNAME << " -d <input_file>\n";
+  std::cout << "- " << EXECUTE << " -d <input_file>\n";
   std::cout << "    input_file  : path and filename (with ending) of the file to be decrypted\n";
   std::cout << "                  <key> is assumed to be default, which is <" << STD_KEY_DIR << STD_KEY_INV << ">.\n";
   std::cout << "                  <output_file> is assumed to be <input_file> without the suffix <.tur>.\n";
 }
 
 void syntaxGenerateKey() {
-  std::cout << "- ./" << PROJECTNAME << " genKey <file> <length> <rotors>\n";
+  std::cout << "- " << EXECUTE << " genKey <file> <length> <rotors>\n";
   std::cout << "    file        : path and filename (without ending) to write the files into\n";
   std::cout << "    length      : length of the key, recommended to use length 8 or larger\n";
   std::cout << "    rotors      : all names of available rotornames (1 character each) without spaces\n";
-  std::cout << "- ./" << PROJECTNAME << " genKey <file> <length>\n";
+  std::cout << "- " << EXECUTE << " genKey <file> <length>\n";
   std::cout << "    file        : path and filename (without ending) to write the files into\n";
   std::cout << "    length      : length of the key, recommended to use length 8 or larger\n";
   std::cout << "                  Assumes <rotors> to be all rotors that can be found in <" << STD_ROT_DIR << ">.\n";
@@ -131,12 +131,12 @@ void syntaxGenerateKey() {
 }
 
 void syntaxGenerateRotors() {
-  std::cout << "- ./" << PROJECTNAME << " genRot <rotor names>\n";
+  std::cout << "- " << EXECUTE << " genRot <rotor names>\n";
   std::cout << "    rotor names : string of all rotor names (1 character each) to be generated.\n";
 }
 
 void syntaxHelp() {
-  std::cout << "- ./" << PROJECTNAME << " <command>\n";
+  std::cout << "- " << EXECUTE << " <command>\n";
   std::cout << "    command     : command you want to see detailed information about\n";
   std::cout << "                  options are: <crypt>, <generate> and <help>\n";
 }
